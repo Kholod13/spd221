c68ee18f3be32704d8d90ba72e39de78a400dd12
Kholod13 <111706893+Kholod13@users.noreply.github.com>
2024-08-19 22:00:36 +0200

    add createCategoryPage

--- pizza-app/app/Http/Controllers/Api/CategoryController.php
+++ pizza-app/app/Http/Controllers/Api/CategoryController.php
@@ -3,14 +3,47 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
 use App\Models\Category;
+use Illuminate\Http\Request;
+use Intervention\Image\Drivers\Gd\Driver;
+use Intervention\Image\ImageManager;
+
 
 class CategoryController extends Controller
 {
+    protected string $upload;
+    protected $sizes = [50,150,300,600,1200];
+
+
+    public function __construct(){
+        $this->upload=env("UPLOAD_DIR");
+    }
+
+
     public function getList(){
         $data = Category::all();
         return response()->json($data)
             ->header('Content-Type', 'application/json', 'charser=utf-8');
+    }
+
+    public function store(Request $request) {
+        $dir = public_path($this->upload);
+        if(!file_exists($dir)) {
+            mkdir($dir,0777);
+        }
+        if($request->hasFile('image') && $request->input("name")!="") {
+            $file = $request->file("image");
+            $fileName = uniqid(). ".webp";
+            $manager = new ImageManager(new Driver());
+            foreach ($this->sizes as $size) {
+                $imageSave = $manager->read($file);
+                $imageSave->scale(width: $size);
+                $path = public_path($this->upload.$size."_".$fileName);
+                $imageSave->toWebp()->save($path);
+            }
+            $item = Category::create(['name'=>$request->input('name'), "image"=> $fileName]);
+            return response()->json(201);
+        }
+        return response()->json("Bad request", 400);
     }
 }
